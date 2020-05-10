86d29d6960b7e1bc2f2267ea2716d0ff48b87e0b
Hannah Ekman <hannah@ekman.cx>
2020-05-09 20:50:47 -0700

    added some comments

--- app/src/main/java/com/example/accalendar/decorators/SpecialDecorator.java
+++ app/src/main/java/com/example/accalendar/decorators/SpecialDecorator.java
@@ -23,7 +23,11 @@ public class SpecialDecorator implements DayViewDecorator {
     @Override
     public boolean shouldDecorate(final CalendarDay day) {
         LocalDate date = day.getDate();
+        // Map contains: (special: Map of events, regular: Map of events)
+        // Special events = events that occur on a certain day of week in a month (ie. 2nd sunday of June)
+        // Regular events = events that occur on a specific date in a month (25th of December)
         for (Map.Entry <String, Map<String, Map<String, Long>>> special : specialDays.entrySet()) {
+            // special.getValue() = event maps: (event: day, month)
             Map<String, Map<String, Long>> specialCategories = special.getValue();
             for(Map.Entry <String, Map<String, Long>> category : specialCategories.entrySet()) {
                 if (isSpecialEvent(category, special.getKey(), date))
@@ -37,13 +41,23 @@ public class SpecialDecorator implements DayViewDecorator {
                                          LocalDate date) {
         Map<String, Long> info = category.getValue();
         if(type.equals("regular")) {
+            // If the date is equal to the event date
             if(date.getMonth().getValue() == info.get("month")
                     && date.getDayOfMonth() == info.get("day"))
                 return true;
-        } else {
-            if (date.getMonth().getValue() == info.get("month") &&
-                    date.getDayOfWeek().getValue() == info.get("day of week")) {
+        } else { //If special
+            if (date.getMonth().getValue() == info.get("month") && // If the date is in the month of the event
+                    date.getDayOfWeek().getValue() == info.get("day of week")) { // If the date is on the correct day of week
                 int daysBetween = date.getDayOfMonth();
+                // By this point we have already determined that this date is the correct day of week,
+                // and is in the correct month. So now we have to determine if it falls on the correct
+                // week of the month
+
+                // General idea of how it works: using week = 2
+                // For the date to be the 2nd day of the month, it must have occurred once before
+                // so there must have been 7 days that must have passed before reaching the 2nd day: daysBetween > 7*(info.get("week")-1
+                // so the date must lie somewhere between the 7th day, and the next 7 days: daysBetween <= 7*info.get("week")
+                // 7 < daysBetween <= 14
                 if (daysBetween <= 7*info.get("week") && daysBetween > 7*(info.get("week")-1))
                     return true;
             }
--- app/src/main/java/com/example/accalendar/decorators/TourneyDecorator.java
+++ app/src/main/java/com/example/accalendar/decorators/TourneyDecorator.java
@@ -34,11 +34,26 @@ public class TourneyDecorator implements DayViewDecorator {
 
     public static boolean isTourney(Map.Entry <String, Map<ArrayList<Long>, Integer>> tourney,
                                     LocalDate date) {
+        // tourney.getValue() = Map(EventMap, saturdayOfMonth)
+        // each map only has one entry, might've been better to use tuples but oh well :P
         Map<ArrayList<Long>, Integer> tourneyInfo = tourney.getValue();
+        // months = EventMap: months(int, monthValue)
+        //      (aka an array of months the event occurs on but firebase stores arrays as maps)
         ArrayList<Long> months = tourneyInfo.keySet().iterator().next();
+        // get the saturdayOfMonth using months as the key
         int saturday = tourneyInfo.get(months);
         Long curMonth = new Long(date.getMonth().getValue());
+        // If the date's month is in the array of months for the event and it is a Saturday
         if(months.contains(curMonth) && date.getDayOfWeek() == DayOfWeek.SATURDAY) {
+            // By this point we have already determined that this date is the correct day of week,
+            // and is in the correct month. So now we have to determine if it falls on the correct
+            // week of the month
+
+            // General idea of how it works: using saturday = 2
+            // For the date to be the 2nd day of the month, it must have occurred once before
+            // so there must have been 7 days that must have passed before reaching the 2nd day: daysBetween > 7*(saturday-1)
+            // so the date must lie somewhere between the 7th day, and the next 7 days: daysBetween <= 7*saturday
+            // 7 < daysBetween <= 14
             int daysBetween = date.getDayOfMonth();
             if (daysBetween <= 7*saturday && daysBetween > 7*(saturday-1))
                 return true;
